32cf63aba7dde5abdcc7c38d5354514e69b5dbfc
GuillermoDeLeon <59849666+GuillermoDeLeon@users.noreply.github.com>
2020-03-19 23:38:43 -0500

    Delete keys.js

--- js/keys.js
+++
@@ -1,6 +0,0 @@
-var lastFmApiKey = "a7ce641458de59957d454e5985113ad1";
-
-// last.fm
-// API key	a7ce641458de59957d454e5985113ad1
-// Shared secret	0c8c3fc2ae3666ccee74952ec0b998f9
-// Registered to	gdeleon210
