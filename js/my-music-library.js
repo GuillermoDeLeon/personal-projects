351aed929a618d43baedf3a20bf8a72d5f173524
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-04-02 22:16:30 -0500

    updates to my-music-library

--- js/my-music-library.js
+++ js/my-music-library.js
@@ -6,7 +6,30 @@ $(document).ready(function() {
 
 });
 
-//api call using ajax to pull info from last.fm
-$.ajax("https://cors-anywhere.herokuapp.com/http://www.last.fm/api/auth/?api_key=" + lastFmApiKey).done(function (data) {
+// api call using ajax to pull info from last.fm
+// $.ajax("https://cors-anywhere.herokuapp.com/http://www.last.fm/api/auth/?api_key=" + lastFmApiKey).done(function (data, status) {
+//     console.log("Request status is:  " + status);
+//     console.log(data);
+//     console.log()
+
+//     $.getJSON("http://ws.audioscrobbler.com//2.0/?method=album.getinfo&api_key=" + lastFmApiKey + "&artist=Cher&album=Believe&format=json").done(function (data, status) {
+//         console.log("Request status is: " + status);
+//         console.log(data);
+// });
+
+$.getJSON("http://ws.audioscrobbler.com//2.0/?method=album.getinfo&api_key=" + lastFmApiKey + "&artist=Iron+Maiden&album=Powerslave&format=json").done(function (data, status) {
+    console.log("Request status is: " + status);
     console.log(data);
 });
+
+    // $.getJSON("http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&user=test&api_key=" + lastFmApiKey + "&limit=10&format=json&callback=?", function(json) {
+    //
+    //     var html = '';
+    //     $.each(json.topartists.artist, function(i, item) {
+    //         console.log(item);
+    //         html += "<p><a href=" + item.url + " target='_blank'>" + item.name + " - " + "Play count : " +item.playcount + "</a></p>";
+    //     });
+    //     $('#result').append(html);
+    // });
+// });
+// });
