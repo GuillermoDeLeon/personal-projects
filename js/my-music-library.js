0395e270b37414cc6c47b8b1881dfab1525ec697
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-04-08 23:04:15 -0500

    updates

--- js/my-music-library.js
+++ js/my-music-library.js
@@ -9,7 +9,8 @@ $(document).ready(function() {
 var listener = function(event) {
     alert('You clicked the submit button');
     var userInputAlbumName = document.forms.album.albumName.value;
-    console.log("Album name should output here --> " + userInputAlbumName);
+    var userInputAlbumArtist = document.forms.album.albumArtist.value;
+    console.log("Album name is: --> " + userInputAlbumName + "/ Album Artist is:  --> " + userInputAlbumArtist);
 };
 
 // DOM used with listener
@@ -18,10 +19,10 @@ document.getElementById('submit-album-name').addEventListener('click',listener);
 
 
 //target by ID and assign user's input for AlbutArtist or Band name to variable on 'click'
-$("#btn-submit-album-artist").click(function() {
-    var userInputAlbumArtist = $("#albumArtist").val();
-    console.log(userInputAlbumArtist);
-});
+// $("#btn-submit-album-artist").click(function() {
+//     var userInputAlbumArtist = $("#albumArtist").val();
+//     console.log(userInputAlbumArtist);
+// });
 // END of section that captures user input for Album Artist or Band name.
 
 
@@ -37,25 +38,13 @@ $("#btn-submit-album-artist").click(function() {
 //         console.log(data);
 // });
 
-// $.getJSON("http://ws.audioscrobbler.com//2.0/?method=album.getinfo&api_key=" + lastFmApiKey + "&artist=Iron+Maiden&album=Powerslave&format=json").done(function (data, status) {
-//     console.log("Request status is: " + status);
-//     console.log(data);
-//
-//     var html =  '';
-//     $("#album-info").html("Album info: " + data.album.name + "<br>" +
-//                         "Artist: " + data.album.artist);
-//     $("#album-tracks").html("Artist: " + data.album.artist);
-//
-// });
+$.getJSON("http://ws.audioscrobbler.com//2.0/?method=album.getinfo&api_key=" + lastFmApiKey + "&artist=Iron+Maiden&album=Powerslave&format=json").done(function (data, status) {
+    console.log("Request status is: " + status);
+    console.log(data);
 
-    // $.getJSON("http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&user=test&api_key=" + lastFmApiKey + "&limit=10&format=json&callback=?", function(json) {
-    //
-    //     var html = '';
-    //     $.each(json.topartists.artist, function(i, item) {
-    //         console.log(item);
-    //         html += "<p><a href=" + item.url + " target='_blank'>" + item.name + " - " + "Play count : " +item.playcount + "</a></p>";
-    //     });
-    //     $('#result').append(html);
-    // });
-// });
-// });
+    var html =  '';
+    $("#album-info").html("Album info: " + data.album.name + "<br>" +
+                        "Artist: " + data.album.artist);
+    $("#album-tracks").html("Artist: " + data.album.artist);
+
+});
