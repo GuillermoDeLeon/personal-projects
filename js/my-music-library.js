ae03ee346c350088450cac4bb0bfdb9c61581678
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-04-10 21:52:50 -0500

    user input passed to api as arguments and pulls information

--- js/my-music-library.js
+++ js/my-music-library.js
@@ -5,46 +5,22 @@ $(document).ready(function() {
     // alert( 'The DOM has finished loading!' );
 });
 
-// function assigned to listener variable when user submits AlbumName, uses forms collection with DOM
-var listener = function(event) {
-    alert('You clicked the submit button');
-    var userInputAlbumName = document.forms.album.albumName.value;
-    var userInputAlbumArtist = document.forms.album.albumArtist.value;
+function getUserInput() {
+    var userInputAlbumArtist = document.getElementById('albumArtist').value;
+    var userInputAlbumName = document.getElementById('albumName').value;
+    // alert("value is: " + userInputAlbumArtist + " and " + userInputAlbumName);
     console.log("Album name is: --> " + userInputAlbumName + "/ Album Artist is:  --> " + userInputAlbumArtist);
-};
+    getAlbumInfo(userInputAlbumArtist, userInputAlbumName);
+}
 
-// DOM used with listener
-document.getElementById('submit-album-name').addEventListener('click',listener);
-// END of section that is used to capture Album Name information submitted by end user for search
+function getAlbumInfo(artistName, albumTitle) {
+    $.getJSON("http://ws.audioscrobbler.com//2.0/?method=album.getinfo&api_key=" + lastFmApiKey + "&artist=" + artistName + "&album=" + albumTitle + "&format=json").done(function (data, status) {
+        console.log("Request status is: " + status);
+        console.log(data);
 
-
-//target by ID and assign user's input for AlbutArtist or Band name to variable on 'click'
-// $("#btn-submit-album-artist").click(function() {
-//     var userInputAlbumArtist = $("#albumArtist").val();
-//     console.log(userInputAlbumArtist);
-// });
-// END of section that captures user input for Album Artist or Band name.
-
-
-
-// api call using ajax to pull info from last.fm
-// $.ajax("https://cors-anywhere.herokuapp.com/http://www.last.fm/api/auth/?api_key=" + lastFmApiKey).done(function (data, status) {
-//     console.log("Request status is:  " + status);
-//     console.log(data);
-//     console.log()
-
-//     $.getJSON("http://ws.audioscrobbler.com//2.0/?method=album.getinfo&api_key=" + lastFmApiKey + "&artist=Cher&album=Believe&format=json").done(function (data, status) {
-//         console.log("Request status is: " + status);
-//         console.log(data);
-// });
-
-$.getJSON("http://ws.audioscrobbler.com//2.0/?method=album.getinfo&api_key=" + lastFmApiKey + "&artist=Iron+Maiden&album=Powerslave&format=json").done(function (data, status) {
-    console.log("Request status is: " + status);
-    console.log(data);
-
-    var html =  '';
-    $("#album-info").html("Album info: " + data.album.name + "<br>" +
-                        "Artist: " + data.album.artist);
-    $("#album-tracks").html("Artist: " + data.album.artist);
-
-});
+        var html = '';
+        $("#album-info").html("Album info: " + data.album.name + "<br>" +
+            "Artist: " + data.album.artist);
+        $("#album-tracks").html("Artist: " + data.album.artist);
+    });
+}
